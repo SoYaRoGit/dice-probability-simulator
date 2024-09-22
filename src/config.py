8e381d208ca8bde747b056f58c426769facf08f7
Соколов Ярослав <yari.sokolov.2004@gmail.com>
2024-09-22 10:42:07 +0300

    Реализовать валидацию входных данных

--- src/config.py
+++ src/config.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
 
 
 class InputModel(BaseModel):
-    """Валидация входных данных на заданное число > 0"""
+    """Валидация входных данных на заданное число 10_000_000 >= x > 0"""
 
-    counter: PositiveInt
+    counter: Annotated[
+        int,
+        Field(
+            title="Счетчик",
+            description="Количество подкидываний игральной кости",
+            ge=1,
+            le=10_000_000,
+        ),
+    ]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
-def main() -> None: ...
+from config import InputModel
+
+
+def main() -> None:
+    try:
+        user_input = int(input("Введите количество подкидываний: "))
+        user_input_model = InputModel(counter=user_input)
+        counter: int = user_input_model.counter
+        print(counter)
+    except ValueError as e:
+        print(f"[ERROR]: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        print("[INFO]: Программа принудительно остановлена!")
